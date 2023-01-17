20fc6a0943137d805d53254f2d6b353d85a2dcfd
CharaD7 <jijakahn6@gmail.com>
2023-01-17 18:03:21 +0000

    Removed border highlight color for toggleterm. Seemed pretty without the border highlights

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -2,16 +2,16 @@ local status_ok, toggleterm = pcall(require, 'toggleterm')
 if not status_ok then return end
 
 toggleterm.setup {
-  autochdir = true,
+	autochdir = true,
 	direction = 'float', -- could be any of 'vertical' | 'horizontal' | 'tab' | 'float'
-	highlights = {
-		FloatBorder = {
-			guibg = "#61afef",
-		},
-		Normal = {
-			guibg = "#61afef",
-		}
-	},
+	-- highlights = {
+	-- 	FloatBorder = {
+	-- 		guibg = "#61afef",
+	-- 	},
+	-- 	Normal = {
+	-- 		guibg = "#61afef",
+	-- 	}
+	-- },
 	float_opts = {
 		border = 'curved',
 		winblend = 15
