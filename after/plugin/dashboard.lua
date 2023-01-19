9c9eaf59119d385d03879956f3a9c836c116f9ea
CharaD7 <jijakahn6@gmail.com>
2023-01-19 10:00:56 +0000

    Added a TODO flag on buggy dashboard

--- after/plugin/dashboard.lua
+++ after/plugin/dashboard.lua
@@ -1,12 +1,12 @@
 local status_ok, dashboard = pcall(require, 'dashboard')
 if not status_ok then return end
 
-local nvim_exec = vim.api.nvim_exec
+-- TODO: Fix this buggy plugin
 
 dashboard.hide_statusline = 1
 dashboard.session_directory = vim.fn.stdpath('data') .. '/sessions'
 dashboard.custom_center = {
-  { icon = '🔎  ', desc = 'Find File                 ', shortcut = 'SPC f f', action = 'Telescope find_files' },
+	{ icon = '🔎  ', desc = 'Find File                 ', shortcut = 'SPC f f', action = 'Telescope find_files' },
 	{ icon = '   ', desc = 'Recents                   ', shortcut = 'SPC f r', action = 'Telescope oldfiles' },
 	{ icon = '   ', desc = 'Find Word                 ', shortcut = 'SPC f w', action = 'Telescope live_grep' },
 	{ icon = '洛  ', desc = 'New File                  ', shortcut = 'SPC f n', action = 'DashboardNewFile' },
