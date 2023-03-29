2458de677b6f72d6d0269b5557460ba92d9966ce
CharaD7 <jijakahn6@gmail.com>
2023-03-29 08:31:42 +0000

    Removed Nvim transparent deprecated configs

--- after/plugin/transparent.lua
+++ after/plugin/transparent.lua
@@ -3,7 +3,5 @@ local status_ok, transparent = pcall(require, 'transparent')
 if not status_ok then return end
 
 transparent.setup {
-	enable = false, -- boolean: enable transparent
 	extra_groups = { "all" },
-	exclude = {}, -- table: groups you don't want to clear
 }
