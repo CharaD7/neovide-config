c0d67dc97f3bae37675ab10d172bc520805b97ba
CharaD7 <jijakahn6@gmail.com>
2023-04-20 12:33:49 +0000

    Changed import of snippets

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,16 +1,13 @@
 local status_ok, luasnip = pcall(require, 'luasnip')
 if not status_ok then return end
 
-local typescript_snippets = '../../snippets/typescript'
-local python_snippets = '../../snippets/python'
-local rust_snippets = '../../snippets/rust'
-
 local snips = function(opts)
   luasnip.config.set_config(opts)
 
   -- vscode format
   require('luasnip.loaders.from_vscode').lazy_load()
-  require('luasnip.loaders.from_vscode').lazy_load { paths = vim.g.vscode_snippets_path or { typescript_snippets, python_snippets, rust_snippets } }
+  require('luasnip.loaders.from_vscode').lazy_load { paths = vim.g.vscode_snippets_path or
+      '' }
 
   -- lua format
   require('luasnip.loaders.from_lua').lazy_load()
